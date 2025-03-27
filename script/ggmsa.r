729fcf401bd19a37867d3bcbd34bb2e76f4368b2
Zihao Zhu <113609109+zihaozhu92@users.noreply.github.com>
2025-03-27 22:59:55 +0100

    Update ggmsa.r

--- script/ggmsa.r
+++ script/ggmsa.r
@@ -1,15 +1,19 @@
+##ggmsa
+##http://yulab-smu.top/ggmsa/articles/ggmsa.html
+
 library(Biostrings)
 library(ggmsa)
 library(ggplot2)
 
 #for Figure2B HvCO1 dna sequences
-aln<-readAAMultipleAlignment(dna_sequences)
+dna_sequences <- system.file("extdata", "xxx_msa4ggmsa.fasta", package = "ggmsa")
 ggmsa(dna_sequences,char_width=0.05,seq_name=F,
             font=NULL,color="Zappo_NT",
             border=NA)+geom_msaBar()
 
 #for Figure3B HvCMF4 and FigureS3C HvCMF1 amino acid sequences
-Strand_color <-read.csv("strand_code.csv",stringsAsFactors = FALSE)
+protein_sequences <- system.file("extdata", "xxx_msa4ggmsa.fasta", package = "ggmsa")
+Strand_color <-read.csv("ggmsa_strand_code.csv",stringsAsFactors = FALSE)
 head(Strand_color)
 Strand_color$color <- trimws(Strand_color$color)
 plot<-ggmsa(protein_sequences,char_width=0.05,seq_name=F,
